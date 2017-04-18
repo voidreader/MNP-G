0992a7531a59806e50330a88e6b60acf17eb60c4
radiogaga <radiogaga.jin@gmail.com>
2017-04-18 18:13:21 +0900

    운영 서버 업데이트
    운영 서버 업데이트

---
+++ Server/Operation/Operation/model/DBAgent.js
@@ -0,0 +1,126 @@
+﻿var fs = require( 'fs' );
+var xmlTojs = require( 'xml2js' );
+var mysql = require( 'mysql' );
+var queryUtil = require('../utils/QueryUtil');
+var log = require('../utils/LogUtil');
+/*
+const SUCCESS = 0;
+const FAIL = 1;
+
+exports.SUCCESS = SUCCESS;
+exports.FAIL = FAIL;
+*/
+const AccountDB = 0;
+const GameDB = 1;
+const GameLog = 2;
+const EnvDB = 3;
+const OP = 4;
+
+exports.AccountDB = AccountDB;
+exports.GameDB = GameDB;
+exports.GameLog = GameLog;
+exports.EnvDB = EnvDB;
+exports.OP = OP;
+
+// ----------------------------------------------------------
+// DB agent util
+// ----------------------------------------------------------
+//var DBConnection = null;
+var DBConnectionPool = null;
+
+function Callback_ProcessDBConnection( err, result ) {
+	var dbInfo = result.DBConnection;
+
+	DBConnectionPool = new Array();
+	DBConnectionPool[AccountDB] = mysql.createPool( dbInfo.AccountDB );
+	DBConnectionPool[GameDB] = mysql.createPool( dbInfo.GameDB );
+	DBConnectionPool[GameLog] = mysql.createPool( dbInfo.GameLog );
+    DBConnectionPool[EnvDB] = mysql.createPool(dbInfo.EnvDB);
+    DBConnectionPool[OP] = mysql.createPool(dbInfo.OP);
+
+	var ret = {
+		connectionLimit : dbInfo.GameDB.connectionLimit
+		, host : dbInfo.GameDB.host
+		, port : dbInfo.GameDB.port
+		, database : dbInfo.GameDB.database
+	};
+	log.write(99, 'GameDB information', ret);
+
+	
+}
+
+function LoadData() {
+	fs.readFile( __dirname + '/DB/DB_Connection.xml', 'utf8', function ( err, data ) {
+		if ( err ) {
+			log.write(1, 'Load Fail DB_Connection', '');
+			process.exit( 1 );
+			return;
+		}
+
+		parser = new xmlTojs.Parser( { mergeAttrs: true, explicitArray: false } );
+		parser.parseString( data, Callback_ProcessDBConnection );
+	});
+}
+exports.LoadData = LoadData;
+
+
+// ----------------------------------------------------------
+// DB Query
+// ----------------------------------------------------------
+// Multiple statement queries
+function DBQuery_Multiple(dbindex, query, callback) {
+	Query( dbindex, query, function ( err, rows ) {
+		callback(err, rows);
+	});
+}
+exports.DBQuery_Multiple = DBQuery_Multiple;
+
+function Query( dbIndex, query, callback ) {
+	if ( dbIndex < 0 || dbIndex >= DBConnectionPool.length ) {
+		log.write(2, 'Invalid DBIndex', dbIndex);
+		return;
+	}
+
+	DBConnectionPool[dbIndex].getConnection( function(err,connection) {
+		if(err) {
+			log.write(1, err, err.message);
+			return;
+		}
+
+		connection.beginTransaction(function() {
+			connection.query( query, function ( err, result ) {
+				//console.log(err, result);
+				if(err) {
+					return connection.rollback(function() {
+						var ret = {
+							err : err
+							, message : err.message
+						};
+						log.write(1, query, ret);
+						//throw err;
+						// db에러가 나더라도 서버를 죽이지 않도록 한다. 로그는 필수 확인
+						return;
+					});
+				}
+
+				connection.commit(function(err) {
+					if (err) {
+						return connection.rollback(function() {
+							log.write(1, err, err.message);
+							//throw err;
+							// db에러가 나더라도 서버를 죽이지 않도록 한다. 로그는 필수 확인
+							return;
+						});
+					}
+
+					callback(err, result);
+				});
+
+				connection.release();
+			});
+		});
+	});
+}
+exports.Query = Query;
+
+
---
+++ Server/Operation/Operation/model/NetworkAgent.js
@@ -0,0 +1,50 @@
+﻿var crypto = require( 'crypto' );
+var request = require('request');
+
+const SUCCESS = 0;
+const FAIL = 1;
+
+exports.SUCCESS = SUCCESS;
+exports.FAIL = FAIL;
+
+function Response( res, result, error, data, token ) {
+
+	var ret = {
+		result: result,
+		error: error,
+		data: data
+	};
+
+	var output;
+	if( token != '' ) {
+		// stringify : java object 를 JSON 으로.
+		// parse : JSON을 java object로
+		var json = JSON.stringify( ret );
+		var cipher = crypto.createCipher( 'aes192', token );
+		output = cipher.update( json, 'utf8', 'base64' );
+
+		output += cipher.final( 'base64' );
+	}
+
+	if( res != undefined ) {
+
+		res.send( ret );  // 암호화 안한 패킷
+		//res.send(output);  // 암호화한 패킷
+
+	}
+}
+exports.Response = Response;
+
+function Post(URLEncodedForms, callback) {
+	request.post( URLEncodedForms, function(err, res, body) {
+		callback(err, res, body);
+	});
+}
+exports.Post = Post;
+
+function Get(URL, callback) {
+	request.get( URL, function(req, message, res) {
+		callback( req, message, res );
+	});
+}
+exports.Get = Get;
