eb6c023cba83ef5aeb0d4a4c94269564b7ff761c
radiogaga <radiogaga.jin@gmail.com>
2017-04-18 11:33:27 +0900

    불필요한 소스 제거
    불필요한 소스 제거

--- MNP-G/Assets/02. Scripts/UI/Component/Gatcha/GatchaModeCtrl.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GatchaModeCtrl : MonoBehaviour {
-
-    public void SetDisable() {
-        this.gameObject.SetActive(false);
-    }
-}
